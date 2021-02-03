2bd1ff7bd6458df88f8e919f1710876c6ad36eef
Ramlah08 <77526728+Ramlah08@users.noreply.github.com>
2021-02-03 05:00:53 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,12 @@
 <html>
 <head><font color="WHITE">
-<h1>M E N U</h1>
+<h1>C L O U D - C O M P U T I N G</h1>
 </head>
 <form action="#" style="width:700px class="posisi";>
 	<fieldset class="h"/>
 	<table style="width: 600px;">
 	<body background="image2.jpg">
 
-<ul>
-	<li><a href="file:///D:/TUGAS%2003%20(Pemrograman%20WEB)/menu%20RAMLAH%20C.HTML">Halaman</a></li>
-	<li><a href="file:///D:/TUGAS%2003%20(Pemrograman%20WEB)/FILE22.HTML">Riwayat Pendidikan</a></li>
-	<li><a href="file:///D:/TUGAS%2003%20(Pemrograman%20WEB)/FILE33.HTML">Riwayat Hidup Orang Tua</a></li>
-
-</ul>
 <font color="WHITE">
 <hr><b>MOTIVASI</b>
 <marquee><b><i><font color="RED">
@@ -27,6 +21,7 @@
 </font>
 </center>
 <p>
+	<center>
 <tr>
 <td><b>
 <i>NAMA</i></b></Td>
@@ -36,6 +31,16 @@
 <tr>
 <td><b><i>NIM </i></b></Td>
 <td><i><b> :217 280 091 </i></b></Td>
+</tr>
+<p>
+	<tr>
+<td><b><i>KELAS  </i></b></Td>
+<td><i><b> :C </i></b></Td>
+</tr>
+<p>
+	<tr>
+<td><b><i>SEMESTER  </i></b></Td>
+<td><i><b> :7 </i></b></Td>
 </tr>
 <p>
 <tr>
@@ -55,21 +60,29 @@
 <p>
 <tr>
 <td><b><i>ASAL DAERAH</i></b></Td>
-<td><b><i>: Desa Saptamarga, Kec.Sukamaju, Kab.LUWU UTARA </i></b></Td>
+<td><b><i>: Desa Saptamarga, Kec.Sukamaju, Kab.Luwu Utara </i></b></Td>
 </tr>
 <p>
 <tr>
-<td><b><i>ALAMAT DOMINSILI </i></b></Td>
-<td><b><i>: Jl.Petta Langge Bilalang, PARE-PARE </i></b></Td>
+<td><b><i>ALAMAT DOMINSILI di PAREPARE</i></b></Td>
+<td><b><i>: Jl.Petta Cangge Bilalang, PARE-PARE </i></b></Td>
 </tr>
 <p>
 <tr>
-<td><b><i>HOBI			 </i></b></Td>
-<td><b><i>: Membaca, Jalan-jalan </i></b></Td>
+<td><b><i>SDN 	</i></b></Td>
+<td><b><i>: 166 Saptamarga, Kec.Sukamaju, Kab.Luwu Utara </i></b></Td>
 </tr>
 <p>
 <tr>
-<td><b><i>JUMLAH SAUDARA	 </i></b></Td>
+<td><b><i>MTS  </i></b></Td>
+<td><b><i>: AL-Falah Lemahabang, Kec.Bone-Bone, Kab.Luwu Utara </i></b></Td></tr>
+<p>
+<tr>
+<td><b><i>MAS 	 </i></b></Td>
+<td><b><i>: AL-Falah Lemahabang, Kec.Bone-Bone, Kab.Luwu Utara </i></b></Td></tr>
+<p>
+<tr>
+<td><b><i>Jumlah Saudari	 </i></b></Td>
 <td><b><i>: 3 </i></b></Td></tr>
 <p>
 <tr>
